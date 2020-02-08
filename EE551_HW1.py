0c2a235e62ed40e04c6654fa56f42da09002e49e
HaoweiL <35860755+HaoweiL@users.noreply.github.com>
2020-02-08 12:09:49 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -14,15 +14,24 @@ def numbers_and_strings():
     x = "EE551"
 
     # Assign a string "Stevens" to a variable y
-
+    y = "Stevens"
+    
     # Repeat variable y 5 times
-
+    y * 5
+    
     # What is the length of z?
-
+    z = ""
+    len(z)
+    0
+    
     # Concatenate variable y with string " is good"
-
+    print(y + " is good")
+    
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
-
+    m = "awesome"
+    n = y + " is good"
+    n.replace('good', n)
+    
     return x, y, z, length, m, n
 
 
@@ -33,18 +42,23 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
-
+    substrings = n.split()
+    
     # Get all the items past the first of the third substring
-
+    substring[1:]
+    
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-
+    A = [[1, 4, 5], [6, 10, 11], [12, 17, 38]]
+    
     # Collect the items in the last column of matrix A using list comprehension
-
+    for row in range(3):
+        print(A[row][2])
+        
     # Collect only the even items of the diagonal of matrix A using list comprehension
-
+    
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
@@ -60,22 +74,28 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-
+    f = {"fruit":"apple", "quantity":4, "color":"green"}
+    
     # Get the item in dictionary f that the key "fruit" maps to
-
+    f["fruit"]
+    
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-
+    f["quantity"] += 1
+    
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-
+    a = {"name":{"first name":"Grace", "last name":"Hopper"}, "jobs":["scientisit", "engineer"], "Age": 85}
+    
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-
+    a["jobs"].append("programmer")
+    
     # Get the third job Grace has that you recently added
-
+    a["jobs"][2]
+    
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
 
     return a, f, p, k
